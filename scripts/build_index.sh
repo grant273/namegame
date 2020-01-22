4248cced985f27a430efec6d2e29dec554330f8c
Grant Klich <kgrant17@vt.edu>
2020-01-22 10:10:19 -0500

    Add women to single img/faces repository

--- scripts/build_index.sh
+++ scripts/build_index.sh
@@ -3,10 +3,8 @@
 # Generate js file index of all named 
 
 
-echo "const MALE_DIR = 'img/m/';" > js/nameIndex.js
-echo "const FEMALE_DIR = 'img/m/';" >> js/nameIndex.js
+echo "const FACE_DIR = 'img/faces/';" > js/nameIndex.js
+
 echo "" >> nameIndex.js
 
-ls img/m | perl -e 'use JSON; @in=grep(s/\n$//, <>); print "const MALES = ".encode_json(\@in)."\n";' >> js/nameIndex.js
-
-ls img/f | perl -e 'use JSON; @in=grep(s/\n$//, <>); print "const FEMALES = ".encode_json(\@in)."\n";' >> js/nameIndex.js
+ls img/faces | perl -e 'use JSON; @in=grep(s/\n$//, <>); print "const FACES = ".encode_json(\@in)."\n";' >> js/nameIndex.js
